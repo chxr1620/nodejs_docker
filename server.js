81ae1d5910b5f7ea18deeab081f706916616be20
Chen <chen.xinrong.bevan@vertivco.com>
2017-04-26 14:55:41 +0800

    'test'

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 // server.js
-var http = require('http')
+var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/plain'
-  })
-  res.end('Hello World!')
-})
+  });
+  var d=new Date().getTime();
+  res.end('Hello World!,date:'+d);
+});
 
-server.listen(32770, function( ) {
-  console.log('Docker DEMO with Node.js is running.')
-}) 
+server.listen(6888, function(){
+   var host = server.address().address;
+   var port = server.address().port;
+
+  console.log('Hi Docker host:'+host+';port:'+port);
+}); 
